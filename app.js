8274228d64b634c9d16bb13f5a22d0e8be1455b6
heysulo <sulochana.456@live.com>
2017-11-26 16:46:09 +0530

    Socket.IO Intergration to backend

---
+++ app.js
@@ -0,0 +1,53 @@
+// Environment variables
+const port = process.env.PORT || 3000;
+
+// Imports
+let logger = require('./modules/logger');
+let credentials = require('./modules/credentials');
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+const socketIO = require('./index');
+
+// Setup Logger
+if (!credentials.isDeployed){
+    logger.disableDatabaseWrite();
+}else {
+    logger.enableDatabaseWrite();
+}
+logger.setStatusCodeLength(4);
+logger.setStatusCodes({
+    info : 'INFO',
+    warn : 'WARN',
+    crit : 'CRIT',
+    log : 'LOG'
+});
+logger.setDefaultStatusCodeKey('info');
+
+// Setup Express
+app.set('views', __dirname + '/');
+app.engine('html', require('ejs').renderFile);
+http.listen(port, function(){
+    logger.log('Server started and listening on PORT ' + port);
+});
+
+// Static Files
+app.use('/',express.static(path.join(__dirname, 'public')));
+
+// Middlewares
+app.use('/api',function (req,res,next) {
+    next();
+})
+
+// Routing
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.get('/api/v1/', function(req, res) {
+    res.send({});
+});
+
+module.exports = http;
--- index.js
+++ index.js
@@ -1,40 +1,6 @@
-// Environment variables
-const port = process.env.PORT || 3000;
+const http = require('./app');
+const io = require('socket.io')(http);
 
-// Imports
-let logger = require('./modules/logger');
-let credentials = require('./modules/credentials');
-
-const express = require('express');
-const app = express();
-const path = require('path');
-
-// Setup Logger
-if (!credentials.isDeployed){
-    logger.disableDatabaseWrite();
-}else {
-    logger.enableDatabaseWrite();
-}
-logger.setStatusCodeLength(4);
-logger.setStatusCodes({
-    info : 'INFO',
-    warn : 'WARN',
-    crit : 'CRIT',
-    log : 'LOG'
-});
-logger.setDefaultStatusCodeKey('info');
-
-// Setup Express
-app.set('views', __dirname + '/');
-app.engine('html', require('ejs').renderFile);
-app.listen(port, function(){
-    logger.log('Server started and listening on PORT ' + port);
-});
-
-// Static Files
-app.use('/',express.static(path.join(__dirname, 'public')));
-
-// Routing
-app.get('/', function(req, res) {
-    res.render('index.html');
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
