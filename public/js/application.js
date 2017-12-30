38685006aeb085e440b981dd6de31f10e15e7ed6
heysulo <sulochana.456@live.com>
2017-12-30 13:22:21 +0530

    Fixed inccorect name on Registration page

--- public/js/application.js
+++ public/js/application.js
@@ -353,79 +353,6 @@ app.service('LoadingMaskService',function ($rootScope) {
         }
     };
 });
-app.controller('LoginController',function (
-    $scope,
-    $rootScope,
-    LoadingMaskService,
-    ApplicationService,
-    $timeout,
-    FacebookService,
-    ProfileService,
-    $location
-) {
-    console.log('Login controller loaded');
-    this.authStatus = 'loading';
-
-    LoadingMaskService.deactivate();
-
-    FacebookService.parseXFBML();
-
-    FacebookService.getUserDetails().then((data)=>{
-        if(data.error){
-            this.authStatus = 'unknown';
-        }else if(data.name){
-            ProfileService.validateUser()
-                .then((data)=>{
-                    this.redirectUpOnLogin(data.data);
-                })
-                .catch((error)=>{
-                    this.authStatus = 'unknown';
-                })
-        }else{
-            this.authStatus = 'unknown';
-        }
-    });
-
-    this.userLoggedIn = ()=>{
-        FacebookService.reAuthenticate(false).then(() => {
-            ProfileService.validateUser()
-                .then((data)=>{
-                    this.redirectUpOnLogin(data.data);
-                })
-                .catch((error)=>{
-                    this.authStatus = 'unknown';
-                    console.error(error);
-                    ApplicationService.pushNotification({
-                        title: 'Unable to log you in',
-                        text : 'For some reasons we could not log you in. Please contact administrator for further assistance. Perform a Hard Refresh and try again.',
-                        template : 'error',
-                        autoDismiss : false
-                    })
-                })
-        });
-    };
-
-    this.redirectUpOnLogin = (data)=>{
-        ApplicationService.setLoadingIndicatorStatus('login.statuschecker',`Logging you in as ${data.name}`);
-        switch (data.state){
-            case 'verified':
-                break;
-            case 'guest':
-                $location.path('/registration');
-                break;
-            case 'pending':
-                $location.path('/registration');
-                break;
-            case 'blocked':
-                $location.path('/registration');
-                break;
-            default:
-                console.error('Unknown user state');
-                break;
-        }
-
-    }
-});
 app.controller('NotificationSystemController',function ($scope,$timeout,$interval) {
 
     this.pushNotification = function (notification = {}) {
@@ -526,6 +453,79 @@ function dissmissNotification(id){
     }, 1000);
 
 }
+app.controller('LoginController',function (
+    $scope,
+    $rootScope,
+    LoadingMaskService,
+    ApplicationService,
+    $timeout,
+    FacebookService,
+    ProfileService,
+    $location
+) {
+    console.log('Login controller loaded');
+    this.authStatus = 'loading';
+
+    LoadingMaskService.deactivate();
+
+    FacebookService.parseXFBML();
+
+    FacebookService.getUserDetails().then((data)=>{
+        if(data.error){
+            this.authStatus = 'unknown';
+        }else if(data.name){
+            ProfileService.validateUser()
+                .then((data)=>{
+                    this.redirectUpOnLogin(data.data);
+                })
+                .catch((error)=>{
+                    this.authStatus = 'unknown';
+                })
+        }else{
+            this.authStatus = 'unknown';
+        }
+    });
+
+    this.userLoggedIn = ()=>{
+        FacebookService.reAuthenticate(false).then(() => {
+            ProfileService.validateUser()
+                .then((data)=>{
+                    this.redirectUpOnLogin(data.data);
+                })
+                .catch((error)=>{
+                    this.authStatus = 'unknown';
+                    console.error(error);
+                    ApplicationService.pushNotification({
+                        title: 'Unable to log you in',
+                        text : 'For some reasons we could not log you in. Please contact administrator for further assistance. Perform a Hard Refresh and try again.',
+                        template : 'error',
+                        autoDismiss : false
+                    })
+                })
+        });
+    };
+
+    this.redirectUpOnLogin = (data)=>{
+        ApplicationService.setLoadingIndicatorStatus('login.statuschecker',`Logging you in as ${data.name}`);
+        switch (data.state){
+            case 'verified':
+                break;
+            case 'guest':
+                $location.path('/registration');
+                break;
+            case 'pending':
+                $location.path('/registration');
+                break;
+            case 'blocked':
+                $location.path('/registration');
+                break;
+            default:
+                console.error('Unknown user state');
+                break;
+        }
+
+    }
+});
 app.controller('PageHeaderController',function ($scope, $timeout, $mdSidenav) {
 
     $scope.pageHeaderVisibility = false;
@@ -652,7 +652,11 @@ app.controller('RegistrationController',function (
     };
 
     this.getBatchLabel = function (indexNumber = 0) {
-        return `${this.ranker(parseInt($scope.loggedInUser.indexNumber.toString().substring(0,2)) - 2)} Batch`
+        if (!!$scope.loggedInUser.indexNumber){
+            return `${this.ranker(parseInt($scope.loggedInUser.indexNumber.toString().substring(0,2)) - 2)} Batch`;
+        }else{
+            return 'Unknown';
+        }
     };
 
     this.checkIndexNumberValidity = function (indexNumber = 0) {
