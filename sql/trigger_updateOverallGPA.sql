9313e0c633bab9b86ae7484bac4ce90d1a11b239
heysulo <sulochana.456@live.com>
2018-01-06 21:49:22 +0530

    Trigger update to support rank and gpa difference

--- sql/trigger_updateOverallGPA.sql
+++ sql/trigger_updateOverallGPA.sql
@@ -4,8 +4,9 @@ CREATE TRIGGER `updateOverallGPA`
     BEFORE UPDATE ON `undergraduate`
     FOR EACH ROW
 BEGIN
+	DECLARE calculatedGpa FLOAT;
     IF (OLD.updated_date = NEW.updated_date) THEN
-		SET NEW.gpa = (
+		SET calculatedGpa = (
 														(coalesce(NEW.y1s1_gpa, 0) * coalesce(NEW.y1s1_credits, 0)) +
 														(coalesce(NEW.y1s2_gpa, 0) * coalesce(NEW.y1s2_credits, 0)) +
 														(coalesce(NEW.y2s1_gpa, 0) * coalesce(NEW.y2s1_credits, 0)) +
@@ -24,6 +25,8 @@ BEGIN
 														coalesce(NEW.y4s1_credits, 0) + 
 														coalesce(NEW.y4s2_credits, 0)
 													);
+                                                    
+        SET NEW.gpa = calculatedGpa;
 		SET NEW.credits = (
 														coalesce(NEW.y1s1_credits, 0) + 
 														coalesce(NEW.y1s2_credits, 0) + 
@@ -45,7 +48,15 @@ BEGIN
 														coalesce(NEW.y4s2_credits_non_gpa, 0)
 													);
 	    SET NEW.updated_date = NOW();
-        SET NEW.gpa_diff = NEW.gpa - coalesce(OLD.gpa, 0);
+        
+        IF (OLD.gpa != calculatedGpa) THEN
+			SET NEW.gpa_diff = calculatedGpa - coalesce(OLD.gpa, 0);
+		END IF;
+        
+        IF (OLD.rank != NEW.rank) THEN
+			SET NEW.rank_diff = coalesce(NEW.RANK, 0) - coalesce(OLD.rank, 0);
+		END IF;
+        
     END IF;
 END//
 DELIMITER ;
