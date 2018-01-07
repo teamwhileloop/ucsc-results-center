ef8c0bc9540dd42bfaa609a821e8607b9cac3c0d
heysulo <sulochana.456@live.com>
2018-01-07 12:26:21 +0530

    Database reconnection bug fix

--- modules/database.js
+++ modules/database.js
@@ -3,6 +3,7 @@ const logger = require('./logger');
 let credentials = require('./credentials');
 let database = require('./database');
 
+let backOffTime = 1;
 let connection = mysql.createConnection({
     host: credentials.database.host,
     user: credentials.database.username,
@@ -10,22 +11,44 @@ let connection = mysql.createConnection({
     database: credentials.database.database
 });
 
-function reconnect() {
+function exponentialBackOff() {
+    logger.log ? logger.log(`Backing off for ${backOffTime} seconds`) : console.log(`Backing off for ${backOffTime} seconds`);
+    connection.end();
+    setTimeout(function () {
+        reconnect(true);
+        backOffTime *= 2;
+    }, backOffTime * 1000);
+}
+
+function resetExponentialBackOff(start = 1) {
+    backOffTime = start;
+}
+
+function reconnect(recon = false) {
     let databaseConnectionTime = new Date();
+    connection.destroy();
+    connection = mysql.createConnection({
+        host: credentials.database.host,
+        user: credentials.database.username,
+        password: credentials.database.password,
+        database: credentials.database.database
+    });
     connection.connect(function(err) {
         if (err){
-            logger.log('Unable to connect to the database after ' + logger.timeSpent(databaseConnectionTime),'crit',true);
+            logger.log('Unable to connect to the database after ' + logger.timeSpent(databaseConnectionTime),'crit',!recon);
+            exponentialBackOff();
         }else{
-            logger.log('Connected to the database in ' + logger.timeSpent(databaseConnectionTime));
+            resetExponentialBackOff();
+            logger.log('Connected to the database in ' + logger.timeSpent(databaseConnectionTime), 'info', true);
         }
     });
 }
 
 connection.on('error', function(err) {
-    logger.log('Error occurred in the database connection. ' + err.code, 'WARN', true, err);
+    logger.log('Error occurred in the database connection. ' + err.code, 'WARN', false, err);
     logger.log('Attempting to reconnect to the database', 'INFO', true, err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-        reconnect();
+        reconnect(true);
     }
 });
 
