7236fc344cde62a091d9dfe3dda54841f86d4847
Sulochana Kodituwakku <sulochana.456@live.com>
2018-02-11 01:11:37 +0530

    Finalized frontend

--- frontend/app/services/admin-service.service.js
+++ frontend/app/services/admin-service.service.js
@@ -1,11 +1,21 @@
 app.service('AdminService',function ($rootScope, FacebookService, $http, apiClient, $q) {
     return {
-        getUserList: function (state, count, page) {
+        getUserList: function (state, count, page, search = '') {
             let query = '';
             page ? query += `page=${page}&` : null;
             count ? query += `count=${count}&` : null;
+            search ? query += `search=${search}&` : null;
             state ? query += `state=${state}` : null;
             return apiClient.get(`/admin/users?${query}`);
+        },
+        acceptUserRequest: function (fbId) {
+            return apiClient.post(`/admin/users/approve/${fbId}`);
+        },
+        rejectUserRequest: function (fbId) {
+            return apiClient.post(`/admin/users/reject/${fbId}`);
+        },
+        resetUser: function (fbId) {
+            return apiClient.post(`/admin/users/reset/${fbId}`);
         }
     };
 });
