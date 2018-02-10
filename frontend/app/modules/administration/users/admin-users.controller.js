649330e61a642ba904d18b8783e1c9a4e45cafe4
Sulochana Kodituwakku <sulochana.456@live.com>
2018-02-11 01:11:37 +0530

    Finalized frontend

--- frontend/app/modules/administration/users/admin-users.controller.js
+++ frontend/app/modules/administration/users/admin-users.controller.js
@@ -1,5 +1,6 @@
 app.controller('AdminUsersController',function (
     $scope,
+    $mdDialog,
     $rootScope,
     $window,
     LoadingMaskService,
@@ -15,23 +16,20 @@ app.controller('AdminUsersController',function (
         count: 25,
         total: -1,
         loading: true,
+        search: '',
         filter: 'e'
     };
     $scope.userList = [];
 
-    if (loggedInUser.state !== 'verified'){
-        $location.path('access-denied');
-    }else{
+    console.log('AdminUsers loaded');
+
+    if (loggedInUser.power >= 50){
         LoadingMaskService.deactivate();
         ApplicationService.hideNavigationIndicator();
         ApplicationService.displayPageHeader({ search: true});
         ApplicationService.updatePageHeader(loggedInUser);
-    }
-
-    console.log('AdminUsers loaded');
-
-    if (loggedInUser.power >= 50){
-        $scope.accessDenied = false;
+    }else{
+        $location.path('access-denied');
     }
 
     getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
@@ -44,14 +42,12 @@ app.controller('AdminUsersController',function (
         scope: $scope
     };
 
-
-
-    this.acceptRequest = function(fbId){
-        console.log('ok', fbId);
-    };
-
     this.setPageCount = function (count) {
         $scope.paginationCtrl.count = count;
+        getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
+    };
+
+    this.refreshList = function () {
         getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
     };
 
@@ -84,9 +80,119 @@ app.controller('AdminUsersController',function (
         }
     };
 
+    $scope.acceptRequest = function(fbUser){
+        let confirm = $mdDialog.confirm()
+            .title('Accept Request ?')
+            .textContent(`Are you sure that you want to accept the request of user ${fbUser.name} ?`)
+            .ok('Accept Request')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function() {
+            acceptRequest(fbUser)
+        }, function() {
+            return 0;
+        });
+    };
+    $scope.rejectRequest = function(fbUser){
+        let confirm = $mdDialog.confirm()
+            .title('Reject Request ?')
+            .textContent(`Are you sure that you want to reject the request of user ${fbUser.name} ?`)
+            .ok('Reject Request')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function() {
+            rejectRequest(fbUser)
+        }, function() {
+            return 0;
+        });
+    };
+    $scope.resetUser = function(fbUser){
+        let confirm = $mdDialog.confirm()
+            .title('Reset Profile ?')
+            .textContent(`Are you sure that you want to reset the profile of user ${fbUser.name} ?`)
+            .ok('Reset Profile')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function() {
+            resetUser(fbUser)
+        }, function() {
+            return 0;
+        });
+
+    };
+
+    $scope.keyPressed = function (keyEvent) {
+        if (keyEvent.keyCode === 13) {
+            getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
+        }
+    };
+
+    function acceptRequest(person) {
+        AdminService.acceptUserRequest(person.id)
+        .then((response)=>{
+            if (response.data.success){
+                ApplicationService.pushNotification({
+                    title: 'Request Accepted',
+                    text : `Request of ${person.name} accepted. A confirmation email has being sent`,
+                    template : 'info',
+                    autoDismiss : true
+                });
+            }else{
+                ApplicationService.pushNotification({
+                    title: 'Failed to Accept Request',
+                    text : response.data.message,
+                    template : 'error',
+                    autoDismiss : false
+                });
+            }
+            getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
+        })
+    }
+
+    function rejectRequest(person) {
+        AdminService.rejectUserRequest(person.id)
+        .then((response)=>{
+            if (response.data.success){
+                ApplicationService.pushNotification({
+                    title: 'Request Rejected',
+                    text : `Request of ${person.name} rejected. `,
+                    template : 'info',
+                    autoDismiss : true
+                });
+            }else{
+                ApplicationService.pushNotification({
+                    title: 'Failed to Reject Request',
+                    text : response.data.message,
+                    template : 'error',
+                    autoDismiss : false
+                });
+            }
+            getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
+        })
+    }
+
+    function resetUser(person) {
+        AdminService.resetUser(person.id)
+        .then((response)=>{
+            if (response.data.success){
+                ApplicationService.pushNotification({
+                    title: 'User Reset Completed',
+                    text : `Profile of ${person.name} has being reset. `,
+                    template : 'info',
+                    autoDismiss : true
+                });
+            }else{
+                ApplicationService.pushNotification({
+                    title: 'Failed to Reset User Profile',
+                    text : response.data.message,
+                    template : 'error',
+                    autoDismiss : false
+                });
+            }
+            getUsers($scope.paginationCtrl.filter, $scope.paginationCtrl.count ,$scope.paginationCtrl.page);
+        })
+    }
+
     function getUsers(state, count, page) {
         $scope.paginationCtrl.loading = true;
-        AdminService.getUserList(state, count, page)
+        AdminService.getUserList(state, count, page, $scope.paginationCtrl.search)
             .then((data)=>{
                 $scope.userList = [];
                 $scope.userList = data.data.data;
