fb28c209b17a79fbfd6d119b0c19210af3a87c6c
Sulochana Kodituwakku <sulochana.456@live.com>
2018-12-16 09:32:56 +0530

    Frontend changes and redirect improvements

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use('/cdn',express.static(path.join(__dirname, 'node_modules')));
 // Routing
 app.get('/', function(req, res) {
     // Redirect HTTPS traffic to HTTPS on production environment
-    if (credentials.isDeployed && !req.secure || req.headers.host !== 'www.ucscresult.com'){
+    if (credentials.isDeployed && (!req.secure || req.headers.host !== 'www.ucscresult.com')){
         res.writeHead(302, {
             'Location': 'https://www.ucscresult.com'
         });
